3395ea753156e1fc6283067da4fd2488ee8ef795
Nika Mamaladze <n_mamaladze@cu.edu.ge>
2022-02-27 12:45:05 +0400

    added logos for cool points

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import React, { useEffect, useState } from "react";
-import "./App.css"
+import "./App.css";
 import Form from "./Form";
 import Card from "./Card";
+import logo from "./github.png";
+import octocat from "./Octocat.png";
+
 function App(){
 
     const [name, setName] = useState("");
@@ -19,7 +22,7 @@ function App(){
         })
     }, [])
 
-    const setData = ({name,avatar_url, profile_url}) => {
+    const setData = ({name,avatar_url}) => {
         setName(name);
         setAvatar(avatar_url);
        
@@ -45,14 +48,22 @@ function App(){
     }
 
     return (
-        <div>
+        <div >
+            <div className="bodyWrapper"><img className="logo" src={logo}/></div>
             
             <div className="search">
+                
                 <Form handleSubmit={handleSubmit} handleChange={handleSearch}  value={name} name={name}/>
                 
             </div>
-            {error ? (<h1>{error}</h1>) : (<div className="card">
-                <Card newPage={`https://github.com/${userInput}`} src={avatar} name={name}  />
+            {error ? (
+                <div className="wrapper">
+                    <h1 className="errorH1">{error}</h1>
+                    <img className="octocat" src={octocat}/>
+                </div>
+                ) : (
+                <div className="card">
+                    <Card newPage={`https://github.com/${userInput}`} src={avatar} name={name}  />
             </div>)}
             
         </div>
